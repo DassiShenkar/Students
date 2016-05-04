3ccc201905105220c780e0bd32e1c99ef34b8cbc
dassi <dassi.shenkar@gmail.com>
2016-05-05 00:59:34 +0300

    added config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    events: {
+        GET_GRADE_BY_ID: 'getGradeByID',
+        GET_TOP_STUDENTS: 'getTopStudents',
+        GET_TOP_STUDENTS_BY_GENDER: 'getTopStudentsByGender'
+    }
+};
