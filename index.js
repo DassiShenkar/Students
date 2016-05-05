363b8c6f5858d169373723fd94e3c556f338ba62
dassi <dassi.shenkar@gmail.com>
2016-05-05 05:48:10 +0300

    wrote function calls for index.js/Users/dassir/school/ws/Students/index.js

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
 
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var connection = mongoose.connect('mongodb://db_usr:db_pass@ds023530.mlab.com:23530/grades_d'); 
+var studentsService = require('./students_ws');
+var students = require('./student');
+var service = null;
 var port = process.env.PORT || 3000;
+
+mongoose.connection.once('open', function() {
+  students.find({}, function(err, data) {
+    if(err) {
+      throw err;
+    }
+    service = new studentsService(data);
+    mongoose.disconnect();
+  });
+});
 
 app.set('json spaces', 4);
 app.use(express.static('views'));
@@ -11,21 +26,21 @@ app.get('/', function(request, response, next) {
   request.next();
 });
 
-app.param('studentID', function(request, response, next, value) {
+app.param('/student', function(request, response, next, value) {
   next();
 });
 
 app.get('/student/:studentID', function(request, response) {
-  var id = request.param.studentID;
-  response.status(200).json({studentID: "1", grade: "90"});
+  response.status(200).json(service.getGradeByID(request.params.studentID));
 });
 
 app.get('/topStudents', function(request, response) {
-  response.status(200).json({studentID: "1", firstName: "Yeal", lastName: "Levi", gender: "female", grade: "90"});
+  response.status(200).json(service.getTopStudents());
 });
 
-app.get('/topStudents/female', function(request, response) {
-    response.status(200).json({studentID: "1", firstName: "Yeal", lastName: "Levi", gender: "female", grade: "90"});
+app.get('/topStudents/:gender', function(request, response) {
+  var gender = request.params.gender;
+    response.status(200).json(service.getTopStudentsByGender(gender));
 });
 
 app.get('/error', function(request, response) {
@@ -38,6 +53,6 @@ app.get('*', function(request, response) {
 
 
 app.listen(port);
-console.log('Listening on port ' + port);
+
 
 
---
+++ students_ws.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var _ = require('lodash');
+var express = require('express');
+var EventEmitter = require('events').EventEmitter;
+var eventsConfig = require('./config');
+
+class StudentsQuery extends EventEmitter {
+    constructor(data) {
+        super();
+        this.data = data;
+        this.result = null;
+
+        this.on(eventsConfig.GET_GRADE_BY_ID, function(studentID) {
+            var result = null;
+            result = _.find(this.data, function(student) {
+                return student._id == studentID;
+            });
+            if(result == null) {
+                result = {'Error' :'Student does not exist'};
+            }
+            this.result = result;
+        });
+
+        
+
+        this.on(eventsConfig.GET_TOP_STUDENTS, function() {
+            var result = null;
+            result = _.pickBy(this.data, function(student) {
+                return student.grade >= 95;
+            });
+            if(result == null) {
+                result = {'Error' :'Student does not exist'};
+            }
+            this.result = result;
+        });
+
+
+        this.on(eventsConfig.GET_TOP_STUDENTS_BY_GENDER, function(gender) {
+            var result = null;
+            result = _.pickBy(this.data, function(student) {
+                return student.gender == gender && student.grade >= 95;
+            });
+            if(result == null) {
+                result = {'Error' :'Student does not exist'};
+            }
+            this.result = result;
+        });
+
+    }
+
+    getGradeByID(studentID) {
+        this.emit(eventsConfig.GET_GRADE_BY_ID, studentID);
+        return this.result;
+    }
+    
+    getTopStudents() {
+        this.emit(eventsConfig.GET_TOP_STUDENTS);
+        return this.result;
+    }
+
+    getTopStudentsByGender(gender) {
+        this.emit(eventsConfig.GET_TOP_STUDENTS_BY_GENDER, gender);
+        return this.result;
+    }
+    
+}
+
+module.exports = StudentsQuery;
